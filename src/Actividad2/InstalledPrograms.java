110056063c2f6602621ef92f0ed83fbb2e0f7e2a
SISA0729 <ismael.sisa@educa.madrid.org>
2024-12-17 18:51:19 +0100

    SISA

--- src/Actividad2/InstalledPrograms.java
+++ src/Actividad2/InstalledPrograms.java
@@ -1,4 +1,46 @@
 package Actividad2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class InstalledPrograms {
+    public static void main(String[] args) {
+        String listPrograms;
+
+        System.out.println(System.getProperties());
+        if (System.getProperty("os.name").contains("Windows")) {
+            listPrograms = "cmd.exe /c dir \"C:\\Program files\"";
+        } else {
+            listPrograms = "ls -l /bin";
+        }
+
+
+        ProcessBuilder processBuilder = new ProcessBuilder(listPrograms.split(" "));
+        try {
+            Process process = processBuilder.start();
+
+            InputStream processInputStream = process.getInputStream();
+
+            InputStream errorInputStream = process.getErrorStream();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(errorInputStream));
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+            bufferedReader = new BufferedReader(new InputStreamReader(processInputStream));
+            line = bufferedReader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = bufferedReader.readLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("El proceso no puede ser lanzado.");
+            e.printStackTrace();
+        }
+    }
 }
--- src/Actividad3/EjemploPing.java
+++ src/Actividad3/EjemploPing.java
@@ -1,4 +1,44 @@
 package Actividad3;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class EjemploPing {
+    public static void main(String[] args) {
+        ProcessBuilder pb = null;
+        if (System.getProperty("os.name").contains("Windows")) {
+            pb = new ProcessBuilder("ping", "google.com");
+        } else {
+            pb = new ProcessBuilder("ping", "-c 4", "google.com");
+        }
+
+        try {
+            Process p = pb.start();
+
+            InputStream inputStream = p.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            InputStream error = p.getErrorStream();
+            InputStreamReader errorReader = new InputStreamReader(error);
+            BufferedReader bufferedError = new BufferedReader(errorReader);
+
+            String linea = bufferedError.readLine();
+            while (linea != null) {
+                System.out.println(linea);
+                linea = bufferedError.readLine();
+            }
+
+            linea = bufferedReader.readLine();
+            while (linea != null) {
+                System.out.println(linea);
+                linea = bufferedReader.readLine();
+            }
+
+        } catch (Exception e) {
+            System.out.println("Problemita");
+        }
+
+    }
 }
